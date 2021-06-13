7cd1e5f2c88f511b79ce4cb06272318c3871accf
David <dunleavyd14@gmail.com>
2021-06-12 21:39:15 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+install_packages:
+	./install_pkg.sh
+	./install_aur.sh
+
+install_dmenu:
---
+++ install_aur.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#from octetz github
+echo
+echo "installing aur packages"
+echo
+TEMP_DIR=$(mktemp -d)
+CURRENT_DIR=$(pwd)
+
+# Load extra keys
+# spotify
+gpg --recv-keys 4773BD5E130D1D45
+
+while read AUR_PKG
+do
+  if ! pacman -Q ${AUR_PKG} > /dev/null; then 
+    cd $TEMP_DIR
+    git clone https://aur.archlinux.org/${AUR_PKG}.git
+    cd ${AUR_PKG} && makepkg -si --noconfirm && cd $TEMP_DIR
+  fi
+done < aur_pkglist.txt
